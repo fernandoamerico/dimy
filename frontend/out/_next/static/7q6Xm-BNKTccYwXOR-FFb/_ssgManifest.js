54a5b3bf289b35448e2790ea410af8a9b51cbbcd
Fernando Américo <fernandoamerico2@gmail.com>
2026-08-26 18:08:42 -0300

    fix: untrack frontend/out and skip goreleaser validate to allow dirty tree

--- frontend/out/_next/static/7q6Xm-BNKTccYwXOR-FFb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcontent\u002F[slug]","\u002Fcontent\u002F[slug]\u002F[id]","\u002Fcontent\u002F[slug]\u002Fnova","\u002Fschema\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
